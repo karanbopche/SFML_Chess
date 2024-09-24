75b87b0cef87f42663674a7a99b830e87fefaaa9
karan bopche <karanbopche@gmail.com>
2024-09-25 00:19:20 +0530

    added widget

--- Chess/Src/Widgets/Button.cpp
+++ Chess/Src/Widgets/Button.cpp
@@ -1,22 +1,64 @@
 #include "Button.h"
 
-
-Button::Button(const std::string& text, const sf::Font& font)
+Button::Button()
 {
-    this->text.setString(text);
-    this->text.setFont(font);
-    this->text.setColor(sf::Color::Black);
+    this->text.setFillColor(sf::Color::Red);
+    this->text.setCharacterSize(100);
+    this->background.setFillColor(sf::Color::Blue);
 }
 
-void Button::EventHandler(sf::Event& event, sf::RenderWindow& window)
+Button::Button(const std::string &text, const sf::Font &font) : Button()
 {
-    if(event.type == sf::Event::MouseButtonReleased)
+    this->SetText(text);
+    this->text.setFont(font);
+}
+
+void Button::EventHandler(sf::Event &event)
+{
+    auto buttonRect = this->text.getGlobalBounds();
+    if (event.type == sf::Event::MouseButtonReleased)
     {
-        auto mousePos = sf::Mouse::getPosition(window)
+        if (this->background.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            if (this->onClick)
+                this->onClick();
+    }
+    if (event.type == sf::Event::MouseMoved)
+    {
+        if (this->background.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+        {
+            this->text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+            if (this->onFocus)
+                this->onFocus();
+        }
+        else
+        {
+            this->text.setStyle(sf::Text::Regular);
+        }
     }
 }
 
-void Button::Draw(sf::RenderWindow* window)
+void Button::Draw(sf::RenderWindow *window)
 {
+    window->draw(this->background);
     window->draw(this->text);
+}
+
+void Button::SetPosition(const sf::Vector2f &position)
+{
+    this->background.setPosition(position);
+    this->text.setPosition(position);
+}
+
+void Button::SetText(const std::string &text)
+{
+    this->text.setString(text);
+    const auto &bound = this->text.getGlobalBounds();
+    this->background.setSize(bound.getSize()*1.3f);
+}
+
+void Button::SetOriginToCenter()
+{
+    auto center = this->background.getSize()/2.0f;
+    this->background.setOrigin(center);
+    this->background.setOrigin(center);
 }
--- Chess/Src/Widgets/Button.h
+++ Chess/Src/Widgets/Button.h
@@ -8,21 +8,33 @@ class Button
 {
 public:
     using Callback = std::function<void(void)>;
-    Button(const std::string& text, const sf::Font& font);
+    explicit Button();
+    Button(const std::string &text, const sf::Font &font);
     ~Button() = default;
-    void EventHandler(sf::Event& event, sf::RenderWindow* window);
-    void Draw(sf::RenderWindow* window);
+    void EventHandler(sf::Event &event);
+    void Draw(sf::RenderWindow *window);
+    void SetPosition(const sf::Vector2f &position);
+    void SetText(const std::string &text);
+    void SetOriginToCenter();
 
-    const sf::Vector2f& GetPosition() {return this->text.getPosition();}
-    void SetPosition(const sf::Vector2f& position) { this->text.setPosition(position);}
-    void SetFont(const sf::Font& font) { this->text.setFont(font);}
-    void SetOnClickCallback(Callback callback) {this->onClick = callback;}
-    void SetOnFocusCallback(Callback callback) {this->onFocus = callback;}
+    inline const sf::Vector2f &GetPosition() { return this->text.getPosition(); }
+    inline void SetFont(const sf::Font &font) { this->text.setFont(font); }
+    inline void SetOnClickCallback(Callback callback) { this->onClick = callback; }
+    inline void SetOnFocusCallback(Callback callback) { this->onFocus = callback; }
 
-
+    void SetBackgroundColor(const sf::Color &color) { this->background.setFillColor(color); }
+    void SetTextColor(const sf::Color &color) { this->text.setFillColor(color); }
+    void SetBorderColor(const sf::Color &color) { this->background.setOutlineColor(color); }
+    void SetTextSize(unsigned int size)
+    {
+        this->text.setCharacterSize(size);
+        const auto &bound = this->text.getGlobalBounds();
+        this->background.setSize(bound.getSize());
+    }
 
 protected:
     sf::Text text;
+    sf::RectangleShape background;
     Callback onClick;
     Callback onFocus;
 };
